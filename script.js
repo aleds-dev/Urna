c7a7c752bfb765e1591e3baeb3e946f6e90ccbe1
Alexandre <dev.ads.github@gmail.com>
2021-10-11 12:21:38 -0300

    Atualização da interface

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-// let seuVoto = document.querySelector('.d1-1 span');
-// let cargo = document.querySelector('.d1-2 span');
-// let descricao = document.querySelector('.d1-4');
-// let aviso = document.querySelector('.d2');
-// let lateral = document.querySelector('.d1-1 span');
-// let numeros = document.querySelector('.d1-rigth');
+let seuVoto = document.querySelector('.d1-1 span');
+let cargo = document.querySelector('.d1-2 span');
+let descricao = document.querySelector('.d1-4');
+let aviso = document.querySelector('.d2');
+let lateral = document.querySelector('.d1-right');
+let numeros = document.querySelector('.d1-3');
+
+let etapaAtual = 0;
+
+
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual];
+
+    let numeroHTML = '';
+
+    for (let i = 0; i < etapa.numeros; i++) {
+        numeroHTML += '<div class="numero"></div>';
+        
+    }
+
+    seuVoto.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHTML;
+}
+
+
+
+function clicou(n){
+    alert(n);
+}
+
+function branco(){
+    alert("Branco");
+    
+}
+
+function corrige(){
+    alert("Corrige");
+    
+}
+function confirma(){
+alert("Confirma");
+    
+}
+
+comecarEtapa();
