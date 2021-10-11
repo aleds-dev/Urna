815a0cbcca1e6979773e3b992c0c6295ec167d25
Alexandre <dev.ads.github@gmail.com>
2021-10-10 22:23:00 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: 11111,
+                nome:'Fulano de Tal',
+                partido:'ABC',
+                fotos: [
+                    {url: '11111.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: 22222,
+                nome:'Farinha da Silva',
+                partido:'ABD',
+                fotos: [
+                    {url: '22222.jpg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: 11,
+                nome:'Ciclano de Tal',
+                partido:'ABC',
+                vice:'Qualquer',
+                fotos: [
+                    {url: '11.jpg', legenda:'Prefeito'},
+                    {url: '11-2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 22,
+                nome:'Qualquer da Silva',
+                partido:'ABD',
+                vice: 'Zul',
+                fotos: [
+                    {url: '22.jpg', legenda:'Prefeito'},
+                    {url: '22-2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+]
---
+++ script.js
@@ -0,0 +1,6 @@
+// let seuVoto = document.querySelector('.d1-1 span');
+// let cargo = document.querySelector('.d1-2 span');
+// let descricao = document.querySelector('.d1-4');
+// let aviso = document.querySelector('.d2');
+// let lateral = document.querySelector('.d1-1 span');
+// let numeros = document.querySelector('.d1-rigth');
